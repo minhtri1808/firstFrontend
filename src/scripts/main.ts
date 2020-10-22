7623ca2dfa77fab3e129dc2abcb35fa83f55a3cf
MigTri <trinm1808@gmail.com>
2020-10-23 05:07:50 +0700

    commit-2

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,14 +6,17 @@ import Axios from "axios";
 const menu = ()=>{
 	const menu = document.querySelector("#menuRes");
 	const bot = document.querySelector(".header__bottom");
+	const social = document.querySelector(".footer__Isocial");
 	if(menu){
 		menu.addEventListener("click",(e)=>{
 			e.preventDefault();
 			console.log(1);
 			bot.classList.toggle("active");
+			social.classList.toggle("active");
 		})
 	}
 }
+
 document.addEventListener("DOMContentLoaded", async () => {
 	getSVGs(".svg");
 	Loading();
