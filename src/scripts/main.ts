4f2c28792c9024cdcaf4d9ca67cecbca96786c0b
MigTri <trinm1808@gmail.com>
2020-10-23 03:03:24 +0700

    f

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,54 +2,22 @@ import { getSVGs, Loading } from "./utilities/util";
 import { Fullpage, FullpageOptions } from "./libraries/Fullpage";
 import Axios from "axios";
 
+
+const menu = ()=>{
+	const menu = document.querySelector("#menuRes");
+	const bot = document.querySelector(".header__bottom");
+	if(menu){
+		menu.addEventListener("click",(e)=>{
+			e.preventDefault();
+			console.log(1);
+			bot.classList.toggle("active");
+		})
+	}
+}
 document.addEventListener("DOMContentLoaded", async () => {
 	getSVGs(".svg");
 	Loading();
-	// create instance fullpage
-	const fpOptions: FullpageOptions = {
-		prevEl: ".fp-prev",
-		nextEl: ".fp-next",
-		speed: 800,
-		slideClass: ".fp-slide",
-		dots: true,
-		on: {
-			// event is fired before slide start transition
-			beforeSlideChange: function (
-				currentSlide,
-				nextSlide,
-				currentIndex,
-				nextIndex,
-			) {
-				console.log(currentSlide, nextSlide, currentIndex, nextIndex);
-			},
-			// event is fired after slide end transition
-			afterSlideChange: function (currentSlide, currentIndex) {
-				console.log(currentSlide, currentIndex);
-			},
-		},
-	};
-	const fp = new Fullpage(".fp-container", fpOptions);
-	// method slide to a slide with index
-	fp.slideTo(0);
-	// method get current index of fullpage
-	fp.getIndex();
-	// method allow or not allow scroll to slide fullpage: true = allow, false = not allow
-	fp.canScroll(true);
+	menu();
 });
-
-const fetchData = () => {
-	const formData = new FormData();
-	formData.append("FirstName", "Sơn");
-	formData.append("LastName", "Vũ");
-	Axios.post("/user", formData).then(function (response) {
-		console.log(response);
-	});
-
-	Axios.get("./api/test.json")
-		.then((response) => {
-			return response;
-		})
-		.finally(() => {});
-};
 
 
